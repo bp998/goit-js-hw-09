9494a9814871ec90d7065b547b1273b3c3de6d90
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-04-06 15:42:16 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const btn = document.querySelector('button[type="submit"]');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
 }
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  let position = 1;
+  let timeout = setTimeout(() => {
+    let interval = setInterval(() => {
+      promiseDelay = Number(delay.value) + position * Number(step.value);
+      createPromise(position, promiseDelay);
+      position++;
+      if (position > Number(amount.value)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+      }
+    }, step.value);
+  }, delay.value);
+});
