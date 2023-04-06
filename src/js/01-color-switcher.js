aeec75630e79e11a06a5be52813261ba86f07e0e
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-04-06 14:10:37 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const colorSwitcher = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+};
+
+startBtn.addEventListener('click', colorSwitcher);
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+});
